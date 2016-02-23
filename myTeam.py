4a95b840ece8e3a2e876fae24746237de5cd19b4
Sean Sjahrial <ap@eduroam-169-233-253-221.ucsc.edu>
2016-02-22 19:48:58 -0800

    hi

--- myTeam.py
+++ myTeam.py
@@ -5,6 +5,8 @@
 # purposes. The Pacman AI projects were developed at UC Berkeley, primarily by
 # John DeNero (denero@cs.berkeley.edu) and Dan Klein (klein@cs.berkeley.edu).
 # For more info, see http://inst.eecs.berkeley.edu/~cs188/sp09/pacman.html
+# Hi
+
 
 from captureAgents import CaptureAgent
 import random, time, util
